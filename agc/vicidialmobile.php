18269f4f88e828a88b8f4167657b6fce8c15c255
Abdullah <abdullahjaffer7@gmail.com>
2020-05-17 08:00:32 +0500

    added second select campaign

--- agc/vicidialmobile.php
+++ agc/vicidialmobile.php
@@ -1016,7 +1016,7 @@ echo "<!-- BROWSER: $BROWSER_WIDTH x $BROWSER_HEIGHT     $JS_browser_width x $JS
 
 if ($campaign_login_list > 0)
 	{
-    $camp_form_code  = "<select class='form-control' size=\"1\" name=\"VD_campaign\" id=\"VD_campaign\" onfocus=\"login_allowable_campaigns()\">\n";
+    $camp_form_code  = "<select class='form-control' placeholder='Select campaign' size=\"1\" name=\"VD_campaign\" id=\"VD_campaign\" onfocus=\"login_allowable_campaigns()\">\n";
 	$camp_form_code .= "<option value=\"\"></option>\n";
 
 	$LOGallowed_campaignsSQL='';
@@ -1045,7 +1045,7 @@ if ($campaign_login_list > 0)
 			}
 		else
 			{
-			echo "<select size=1 name=VD_campaign id=VD_campaign onFocus=\"login_allowable_campaigns()\">\n";
+			echo "<select class='form-control' size=1 name=VD_campaign id=VD_campaign onFocus=\"login_allowable_campaigns()\">\n";
 			echo "<option value=\"\">-- "._QXZ("USER LOGIN ERROR")." --</option>\n";
 			echo "</select>\n";
 			}
@@ -1270,7 +1270,7 @@ if ($relogin == 'YES')
     echo "<input class='form-control mb-2' placeholder='User Password' type=\"password\" name=\"VD_pass\" size=\"10\" maxlength=\"20\" value=\"$VD_pass\" /> </div>";
     // echo "<tr><td align=\"right\" valign=\"top\"><font class=\"skb_text\">"._QXZ("Campaign:")."</font>  </td>";
     echo "<font class=\"skb_text\"><span id=\"LogiNCamPaigns\">$camp_form_code</span></font>";
-    // echo "<tr><td align=\"center\" colspan=\"2\"><input type=\"submit\" name=\"SUBMIT\" value=\""._QXZ("SUBMIT")."\" /> &nbsp; \n";
+    // echo "<input type=\"submit\" name=\"SUBMIT\" value=\""._QXZ("SUBMIT")."\" />";
     // echo "<span id=\"LogiNReseT\"><input type=\"button\" value=\""._QXZ("Refresh Campaign List")."\" onclick=\"login_allowable_campaigns()\"></span></td></tr>\n";
     // echo "<tr><td align=\"left\" colspan=\"2\"><font class=\"body_tiny\"><br />"._QXZ("VERSION:")." $version &nbsp; &nbsp; &nbsp; "._QXZ("BUILD:")." $build</font></td></tr>\n";
     echo "</div><div class='col-md-3'></div></div></div>\n";
